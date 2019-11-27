898ba6b3f4b491962be1318fe170143459047427
trincot <51237479+trincot@users.noreply.github.com>
2019-11-27 22:37:27 +0100

    Update lmc.js
    Show values of labels when used as argument (not for branching).
    Allow lowercase mnemonics
    Include original code comments in interface
    Let disassemble return array of array instead of string.
    Syntax highlighting
    Abend when undefined opcode
    Improved CSS styling for more responsive design.

--- lmc.js
+++ lmc.js
@@ -8,9 +8,11 @@ class LMC {
         this.reset();
         // Additional properties to aid disassembly
         this.mailboxName = [];
+        this.comment = [];
         this.codeMailboxes = new Set;
         this.labelLength = 0;
         this.err = null;
+        this.loaded = false;
     }
     /* Sets the inbox property to a callback function that will consume the given iterable */
     setInputFromIterable(arr) {
@@ -45,67 +47,84 @@ class LMC {
         // Clear
         this.mailbox.length = 0;
         this.mailboxName.length = 0;
+        this.comment.length = 0;
         this.codeMailboxes.clear();
         this.labelLength = 0;
         this.err = null;
+        this.loaded = false;
 
-        this.lines = program.match(/^([ \t]*\w+)+/gm); // Ignore comments
-        let lineWords = this.lines.map(line => line.match(/\S+/g) || []);
-        
+        this.lines = program.match(/^[ \t]*\w.*/gm);
+        //this.lines = program.trim().split(/\r?\n/);
+        let lineWords = this.lines.map(line => line.match(/[^\s\w].*|\S+/g) || []);
         // First pass: identify labels
         let labels = {};
-        lineWords.some((words, address) => {
-            if (words[0] in LMC.mnemonics || !isNaN(words[0])) return;
+        for (let [address, words] of lineWords.entries()) {
+            if (words[0].toUpperCase() in LMC.mnemonics || !isNaN(words[0])) continue; // no label here
             let label = words.shift();
-            if (label[0] <= "9") return this.err = { address, msg: "Label cannot start with a digit" };
-            if (labels[label]) return this.err = { address, msg: "Label cannot be defined twice" };
-            labels[label] = address;
+            if (!words.length) return this.err = { address, msg: "'" + label + "' is not a valid mnemonic" };
+            if (label[0] <= "9") return this.err = { address, msg: "'" + label + "' cannot start with a digit" };
+            if (labels[label]) return this.err = { address, msg: "'" + label + "' cannot be defined twice" };
+            labels[label.toUpperCase()] = address;
             this.mailboxName[address] = label;
             this.labelLength = Math.max(this.labelLength, label.length);
-        });
+        }
         // Second pass: resolve symbols
-        if (!this.err) {
-            lineWords.some((words, address) => {
-                let [mnemonic, arg] = words;
-                let syntax = LMC.mnemonics[mnemonic];
-                if (/^\d{1,3}$/.test(mnemonic)) {
-                    syntax = Object.values(LMC.mnemonics).find(({ opcode, arg }) => 
-                        arg === 1 ? mnemonic[0]*100 == opcode : +mnemonic == opcode
-                    ) || { arg: -1 };
-                    arg = +mnemonic - (syntax.opcode || 0);
-                }
-                if (!syntax) return this.err = { address, msg: "Unknown mnemonic '" + mnemonic + "'" };
-                if (arg === undefined && syntax.arg > 0) return this.err = { address, msg: mnemonic + " needs an argument" };
-                if (arg && syntax.arg === 0) arg = 0; // ignore the argument -- it should be treated as a comment
-                let mailbox = arg === undefined ? 0 : isNaN(arg) ? +labels[arg] : +arg;
-                if (Number.isNaN(mailbox)) return this.err = { address, msg: "Undefined label " + arg };
-                if ("opcode" in syntax && (mailbox < 0 || mailbox > 99)) return this.err = { address, msg: "Mailbox must be in the range 0..99" };
-                this.mailbox[address] = (syntax.opcode||0) + mailbox;
-                if (this.mailbox[address] < 0 || this.mailbox[address] > 999) return this.err = { address,  msg: "Out of range value" };
-                if ("opcode" in syntax) this.codeMailboxes.add(address);
-            });
+        for (let [address, words] of lineWords.entries()) {
+            let [mnemonic, arg, more] = words;
+            if (arg && /\W/.test(arg[0])) [arg, more] = [, arg]; 
+            let syntax = LMC.mnemonics[mnemonic.toUpperCase()];
+            if (/^\d{1,3}$/.test(mnemonic)) {
+                syntax = Object.values(LMC.mnemonics).find(({ opcode, arg }) => 
+                    arg === 1 ? mnemonic[0]*100 == opcode : +mnemonic == opcode
+                ) || { arg: -1 };
+                arg = +mnemonic - (syntax.opcode || 0);
+            }
+            if (!syntax) return this.err = { address, msg: "Unknown mnemonic '" + mnemonic + "'" };
+            if (arg === undefined && syntax.arg > 0) return this.err = { address, msg: mnemonic + " needs an argument" };
+            if (arg !== undefined && syntax.arg === 0) return this.err = { address, msg: mnemonic + " should not have an argument" };
+            let mailbox = arg === undefined ? 0 : isNaN(arg) ? +labels[arg.toUpperCase()] : +arg;
+            if (Number.isNaN(mailbox)) return this.err = { address, msg: "Undefined label '" + arg + "'" };
+            if ("opcode" in syntax && (mailbox < 0 || mailbox > 99)) return this.err = { address, msg: "Mailbox must be in the range 0..99" };
+            this.mailbox[address] = (syntax.opcode||0) + mailbox;
+            if (this.mailbox[address] < 0 || this.mailbox[address] > 999) return this.err = { address,  msg: "Out of range value" };
+            if ("opcode" in syntax) this.codeMailboxes.add(address);
+            this.comment[address] = more || "";
         }
         // Initialise calculator & program counter
         this.accumulator = 0;
         this.flag = false;
         this.reset();
+        this.loaded = true;
     }
     /* Gets a text version of the mailbox contents */
     disassembled() {
-        if (this.err) return this.lines;
+        if (!this.loaded) {
+            return this.lines.map((line, address) =>
+                [(address+"").padStart(2, " "),
+                 "".padStart(3, " "),
+                 line,
+                 "", "", "", ""]
+            );
+        }
         return this.mailbox.map((value, address) => {
-            let line = (address+"").padStart(2, "0") + ": " + (value+"").padStart(3, "0") + " "
-                + (this.mailboxName[address] || "").padEnd(this.labelLength, " ") + " ";
+            let line = [(address+"").padStart(2, "0"),
+                        (value+"").padStart(3, "0"),
+                        (this.mailboxName[address] || "").padEnd(this.labelLength, " ")];
             if (this.codeMailboxes.has(address) || address === this.programCounter) {
                 let argument = this.mailboxName[value%100] || (value+"").slice(-2);
                 for (let [mnemonic, { opcode, arg }] of Object.entries(LMC.mnemonics)) {
                     if (value === opcode || arg && value > opcode && value < opcode + 100) {
-                        return line + mnemonic + (arg ? " " + argument : "");
+                        line.push(mnemonic);
+                        line.push((arg ? argument : "").padEnd(this.labelLength, " "));
+                        // Add the value that the argument currently has when its not a branch instruction
+                        line.push((arg && mnemonic[0] !== "B" ? ""+this.mailbox[value%100] : "").padEnd(3, " "), this.comment[address]);
+                        return line;
                     }
                 }
             }
-            return line + "DAT" + (""+value).padStart(4, " ");
-        });
+            line.push("DAT", (""+value).padEnd(this.labelLength, " "), "   ", this.comment[address]);
+            return line;
+        }).filter(Boolean);
     }
     /* Repeatedly performs a step */
     run() {
@@ -166,26 +185,30 @@ class LMC {
             }
         };
         let halt = () => this.programCounter = (this.programCounter + 99) % 100;
-        let functions = [
-            halt,
-            (address) => calculator(mailbox(address), true),
-            (address) => calculator(-mailbox(address), true),
-            (address) => mailbox(address, calculator()),
-            () => null, // NOOP
-            (address) => calculator(mailbox(address), false),
-            (address) => this.programCounter = address,
-            (address) => calculator() === 0 && (this.programCounter = address),
-            (address) => !flag() && (this.programCounter = address),
-            (inout) => inout === 1 ? !isNaN(inputValue = this.inbox()) && log.reads.push("INP") && calculator(inputValue, false)
-                     : inout === 2 ? log.writes.push("OUT") && this.outbox(calculator())
-                     : null // NOOP
-        ];
+        let functions = {
+            0: halt,
+            1: (address) => calculator(mailbox(address), true),
+            2: (address) => calculator(-mailbox(address), true),
+            3: (address) => mailbox(address, calculator()),
+            5: (address) => calculator(mailbox(address), false),
+            6: (address) => this.programCounter = address,
+            7: (address) => calculator() === 0 && (this.programCounter = address),
+            8: (address) => !flag() && (this.programCounter = address),
+          901: () => !isNaN(inputValue = this.inbox()) && log.reads.push("INP") && calculator(inputValue, false),
+          902: () => log.writes.push("OUT") && this.outbox(calculator())
+        };
         // Read instruction
         let content = this.mailbox[this.programCounter] || 0;
+        // Get corresponding function
+        let fun = functions[Math.floor(content / 100)] || functions[content];
+        if (!fun) {
+            this.err = { address: this.programCounter, msg: "Abnormal termination: invalid opcode " + content };
+            return;
+        }
         // Update program counter (wrap around)
         this.programCounter = (this.programCounter + 1) % 100;
         // Execute the instruction
-        functions[Math.floor(content / 100)](content % 100);
+        fun(content % 100);
         if (this.mailbox[this.programCounter] === undefined) this.mailbox[this.programCounter] = 0;
         if (isNaN(inputValue)) halt(); // Input is lacking
         else if (!this.isDone()) return log;
@@ -218,27 +241,27 @@ LMC.createGUI = function(container) {
     let timer, inputTimer, outputTimer, processedInput = [];
     
     let programNode = container.childNodes[0];
-    let program = programNode.nodeValue;
+    let program = programNode.nodeValue.trim();
     if (!/\sHLT\s/i.test(program)) return; // there is no program. Don't do anything
     programNode.remove();
     
     container.insertAdjacentHTML("afterbegin", 
         (container === document.body ? "<style>body, html { margin: 0; height: 100vh }</style>" : "") + `
 <div class="lmc">
-    <div><div><div data-name="code"></div></div></div>
-    <div><table>
-        <tr><td class="lmcLabel">Acc:</td><td><input readonly data-name="acc" size="3">
-            Neg: <input readonly data-name="neg" size="3">
-            <button data-name="reload">Reload</button></td></tr>
-        <tr><td class="lmcLabel">Input:</td><td><input data-name="input"></td></tr>
-        <tr><td class="lmcLabel"><span>Output:</span></td><td><input readonly data-name="output"></td></tr>
-        <tr><td colspan="2">
+    <div data-name="code"></div>
+    <div>
+            <span class="lmcNowrap"><span>Acc:</span><input type="text" readonly data-name="acc" size="3"></span>
+            <span class="lmcNowrap"><span>Neg:</span><input type="text" readonly data-name="neg" size="3"></span>
+        <span class="lmcNowrap"><span>Inp:</span><input type="text" data-name="input"></span>
+        <span class="lmcNowrap"><span>Out:</span><input type="text" readonly data-name="output"></span>
+        <span>
             <button data-name="run">Run</button>
             <button data-name="walk">Walk</button>
-            <button data-name="step">Step</button>
-            </td></tr>
-        <tr><td colspan="2" data-name="err"></td></tr>
-    </table></div>
+            <button data-name="step">Step<small> F8</small></button>
+            <button data-name="reload">Reload</button>
+        </span>
+        <span data-name="err"></span>
+    </div>
 </div>`);
     
     let gui = {};
@@ -246,9 +269,15 @@ LMC.createGUI = function(container) {
         gui[elem.dataset.name] = elem;
     }
 
+    if (program.slice(0, 7) === "#input:") { // Get directive on first line
+        let i = program.search(/\r?\n/);
+        gui.input.value = program.slice(7, i).trim(); // pre-fill the input field.
+        program = program.slice(i).trim();
+    }
+    
     let lmc = new LMC;
-    lmc.load(program);
-
+    let err = lmc.load(program);
+    
     lmc.inbox = function grabInput() {
         function clipLastInputValue() {
             clearInterval(inputTimer);
@@ -291,9 +320,10 @@ LMC.createGUI = function(container) {
     };
     
     gui.run.onclick = () => run(1);
-    gui.walk.onclick = () => run(200);
+    gui.walk.onclick = () => run(400);
     gui.step.onclick = () => run(0);
-
+    document.body.addEventListener("keydown", (e) => e.key === 'F8' && run(0));
+    
     function run(delay) {
         let doStep = () => displayStatus(!lmc.step());
         clearInterval(timer);
@@ -311,7 +341,7 @@ LMC.createGUI = function(container) {
 
     displayStatus(true);
    
-    function displayStatus(andPause) {
+    function displayStatus(andPause=!timer) {
         if (andPause) {
             clearInterval(timer);
             timer = null;
@@ -320,29 +350,35 @@ LMC.createGUI = function(container) {
         let cls = "highlight";
         gui.acc.value = lmc.accumulator;
         gui.neg.value = lmc.flag ? "YES" : "NO";
+        gui.neg.style.backgroundColor = lmc.flag ? "orange" : "";
         let lines = lmc.disassembled();
-        let width = Math.max(...lines.filter(Boolean).map(line => line.length)) + 2;
-        lines = lines.map(line => line && ('<span>' + (" " + line).padEnd(width, " ") + '</span>'));
         if (lmc.err) {
             focusLine = lmc.err.address;
             cls = "error";
             gui.err.textContent = lmc.err.msg;
         }
-        lines[focusLine] = lines[focusLine].replace(">", ' class="' + cls + '">');
-        gui.code.innerHTML = lines.filter(Boolean).join`\n`;
+        let template = "Front>@: @.Label> @ .Mnemo>@ .Arg>@.Inspect>@.@Comment>@."
+            .replace(/\w+/g, '<span class="lmc$&"').replace(/\./g, "</span>");
+        
+        focusLine = lines.findIndex(line => line[0] == focusLine);
+        lines.forEach(line => line.splice(4, 2, line[4].trim(), (line[5].trim() ? "=" : " ") + line[5], line[4].replace(/\w+/, "")));
+        lines = lines.map(line => '<tr class="lmcLine"><td>' 
+            + template.replace(/@/g, () => line.shift().replace(/</g, '&lt;')).trim()
+                      .replace(/(Mnemo)(.*?)(\w+)/, "$1 lmc$3$2$3")
+            + "</td></tr>");
+        lines[focusLine] = lines[focusLine].replace('"', '"' + cls + ' ');
+        
+        gui.code.innerHTML = "<table>" + lines.join`` + "</table>";
         gui.step.disabled = lmc.err || lmc.isDone();
         gui.run.disabled = gui.walk.disabled = lmc.err || lmc.isDone();
         // Scroll highlighted line into view
         let focusSpan = gui.code.querySelector("." + cls);
-        let focusRect = focusSpan.getBoundingClientRect();
-        let codeRect = gui.code.parentElement.getBoundingClientRect();
-        let add = focusRect.bottom - codeRect.bottom;
-        let sub = codeRect.top - focusRect.top;
-        if (add > 0) {
-            gui.code.parentElement.scrollTop += add + 2;
-        } else if (sub > 0) {
-            gui.code.parentElement.scrollTop -= sub + 2;
-        }
+        let focusTop = focusSpan.getBoundingClientRect().top;
+        let codeTop = gui.code.getBoundingClientRect().top;
+        let add = (focusTop + focusSpan.clientHeight) - (codeTop + gui.code.clientHeight);
+        let sub = codeTop - focusTop;
+        if (add > 0) gui.code.scrollTop += add + 2;
+        else if (sub > 0) gui.code.scrollTop -= sub + 2;
     }
 }
 
@@ -354,22 +390,16 @@ document.addEventListener("DOMContentLoaded", function () {
               display: flex;
               flex-direction: row;
               font-family: monospace;
+               align-content: stretch;
             }
+            .lmc table { border-collapse: collapse; width: 100%}
+            .lmc table td { padding: 0 5px 0 5px; }
 
             .lmc>div:first-child {
-              display: flex;
-              flex: 1;
               min-height: 0px;
-            }
-
-            .lmc>div:first-child>div {
-              flex: 1;
+              overflow: auto;
               overflow-y: scroll;
               background-color: #f8f8f8;
-            }
-
-            .lmc>div:first-child>div>div {
-              padding: 5px;
               white-space: pre;
             }
 
@@ -377,20 +407,34 @@ document.addEventListener("DOMContentLoaded", function () {
               padding: 10px;
               background-color: #0B5AB0;
               color: white;
-              width: 100%;
+              flex: 1;
+              overflow-y: auto;
+              min-width: 6em;
+              display: flex;
+              flex-direction: column;
             }
 
-            .lmc>div:last-child td:last-child { width: 100%; }
-            .lmc input { font-family: inherit; border: 0.5px solid; padding-right: 1px; padding-left: 1px;  }
-            .lmc input::placeholder { background-color: yellow;  }
-            .lmc input[readonly] { background-color: #f8f8f8; }
+            .lmc input[type="text"] { font-family: inherit; border: 0.5px solid; padding-right: 1px; padding-left: 1px; margin-bottom: 2px; }
+            .lmc input[type="text"] { font-family: inherit; border: 0.5px solid; padding-right: 1px; padding-left: 1px; }
+            .lmc input::placeholder { background-color: yellow; }
+            .lmc input[readonly] { background-color: #f0f0f0; }
             .lmc input[size="3"] { text-align: right }
-            .lmc input:not([size="3"]) { width: 100% }
-            .lmc button { width: 4em }
-            .lmc .lmcLabel { text-align: right }
+            .lmc input[type="text"]:not([size="3"]) { flex-grow: 1;  width: 100%; min-width: 3em }
+            .lmc button { width: 5em; margin-bottom: 2px; }
+            .lmcNowrap { white-space: nowrap; display: flex; flex-direction: row; align-items: baseline; }
             .lmc .highlight { background: yellow }
-            .lmc .error { background: red }
-            .lmc td { white-space:nowrap }
+            .lmc .error { background: darkorange; font-weight: bold }
+            .lmc [data-name="err"] { color: darkorange; font-weight: bold }
+            .lmcFront { font-size: smaller; color: #aaa }
+            .lmcInspect { font-size: smaller; color: darkorange; vertical-align: text-top; }
+            .lmcComment { font-style: italic; color: darkgreen; }
+            .lmcMnemo { font-weight: bold; }
+            .lmcBRZ, .lmcBRP, .lmcBRA, .lmcHLT { color: darkviolet }
+            .lmcINP, .lmcOUT { color: indianred }
+            .lmcLDA, .lmcSTA, .lmcADD, .lmcSUB { color: navy }
+            .lmcDAT { color: silver }
+            .lmcLabel { color: black }
+            .lmcDAT+span { color: darkred }
         </style>`);
     document.querySelectorAll(".lmcContainer, body").forEach(LMC.createGUI);
-});    
+});
